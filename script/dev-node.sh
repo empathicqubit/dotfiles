68e5bd9eada6f694ac779689cb2cd03a7310e08e
Jessica <jessica@allons.me>
2018-02-09 16:16:18 -0500

    Require device identifier.

--- script/dev-node.sh
+++ script/dev-node.sh
@@ -1,28 +1,30 @@
 #! /bin/bash
 
-if [ -n "$1" ]; then
-    2>/dev/null find -L /sys/class/$1 -maxdepth 2 -mindepth 2 -name uevent -exec realpath "{}" +
-else
-    find /sys/devices -name uevent
-fi | {
-    if [ -n "$2" ]; then
-	readarray -t uevents              
+VENDOR_ID="$(echo "$1" | awk '-F:' '{ print $1 }')"
+PRODUCT_ID="$(echo "$1" | awk '-F:' '{ print $2 }')"
 
-	for u in "${uevents[@]}"; do
-	    path="${u%/uevent}"
-	    while [ "$path" != "/sys/devices" ] && ! [ -f "$path"/idVendor ]; do
-		path="${path%/*}"
-	    done
+if [ -z "$VENDOR_ID" ] || [ -z "$PRODUCT_ID" ] ; then
+    >&2 echo "You must specify a device identifier in the format xxxx:yyyy"
+    exit 1
+fi
 
-	    [ "$path" != "/sys/devices" ] && read readValue < "$path"/idVendor && [ "$readValue" = "$2" ] && {
-		if [ -n "$idProduct" ]; then
-		    read readValue < "$path"/idProduct && [ "$readValue" = "$3" ]
-		fi
-	    } && echo "$u"
-	done
-    else
-	cat
-    fi
+echo "Nodes for $VENDOR_ID:$PRODUCT_ID"
+
+find /sys/devices -name uevent | {
+    readarray -t uevents              
+
+    for u in "${uevents[@]}"; do
+        path="${u%/uevent}"
+        while [ "$path" != "/sys/devices" ] && ! [ -f "$path"/idVendor ]; do
+            path="${path%/*}"
+        done
+
+        [ "$path" != "/sys/devices" ] && read readValue < "$path"/idVendor && [ "$readValue" = "$VENDOR_ID" ] && {
+            if [ -n "$idProduct" ]; then
+                read readValue < "$path"/idProduct && [ "$readValue" = "$PRODUCT_ID" ]
+            fi
+        } && echo "$u"
+    done
 } | {
     readarray -t uevents              
 
