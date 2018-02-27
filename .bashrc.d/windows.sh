d81ef9780e36c81552be6891278670b853d8850a
Jessica <jessica@allons.me>
2018-02-26 19:26:11 -0500

    Commands to handle windows quirks.

--- .bashrc.d/windows.sh
+++ .bashrc.d/windows.sh
@@ -4,5 +4,13 @@ function __main() {
     # Placeholder for WSL, etc.
 }
 
+function docker-machine-env-windows() {
+    eval $(docker-machine.exe env --shell bash "$@" | sed -e 's@\\@/@g' -e 's@"\([a-zA-Z]\):/@"/mnt/\L\1/@g')
+}
+
+function fixcodepage() {
+    cmd.exe /c "chcp 850 && $* && chcp 65001 || chcp 65001"
+}
+
 __main
 unset -f __main;
