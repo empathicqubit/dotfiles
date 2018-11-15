442fbab0bb7f38fd2bf5f50ed345a610cb5c474d
empathicqubit <empathicqubit@allons.me>
2018-11-15 15:56:42 -0800

    tmux pane resize shortcuts

--- .bashrc.d/10-tmux.sh
+++ .bashrc.d/10-tmux.sh
@@ -7,3 +7,19 @@ function tmux() {
         command tmux "$@"
     fi
 }
+
+tmxsync () {
+   export _TMUX_SYNC=1;
+   for w in $(tmux lsw -F '#{window_index}#{window_active}'|sed -ne 's/0$//p'); do
+      tmux joinp -d -b -s $w -v -t $(tmux lsp -F '#{pane_index}'|tail -n 1)
+   done
+   tmux setw synchronize-panes
+}
+tmxunsync () {
+   [ -z "$_TMUX_SYNC" ] && return
+   for p in $(tmux lsp -F '#{pane_index}#{pane_active}' | sed -ne 's/0$//p'); do
+      tmux breakp -d -t 1
+   done
+   unset _TMUX_SYNC
+   tmux setw synchronize-panes
+}
--- .vimrc
+++ .vimrc
@@ -73,6 +73,9 @@ let g:deoplete#enable_at_startup = 1
 let g:deoplete#omni_patterns = {}
 let g:deoplete#omni_patterns.terraform = '[^ *\t"{=$]\w*'
 
+let g:deoplete#omni#input_patterns.scala='[^. *\t]\.\w*'
+
+
 let g:airline#extensions#tabline#enabled = 1
 let g:airline_theme='simple'
 
