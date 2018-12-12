b9a6a58bcb39f86cd381bf67c6e81fde0ca5a2f6
empathicqubit <empathicqubit@allons.me>
2018-12-12 12:19:59 -0800

    Pull stuff. Git aliases.

--- .bashrc.d/20-git.sh
+++ .bashrc.d/20-git.sh
@@ -1,7 +1,25 @@
 #! /bin/bash
 function git {
+
     if [ "$1" == "push" ] ; then
         git log @{upstream}..
+    fi
+
+    local HASHES=
+    local PULLWORKED=
+    if [ "$1" == "pull" ] ; then
+        IFS= read -r HASHES < <(command git pull | tee /dev/stdout | grep -i -o -E '[0-9a-f]{7}\.\.[0-9a-f]{7}')
+
+        PULLSTATUS=$PIPESTATUS[0]
+
+        if ((PULLSTATUS)) ; then
+            echo "Pull failed!"
+            return $PULLSTATUS
+        fi
+
+        echo "Pull succeeded"
+
+        git diff "$HASHES"
     fi
 
     local CAPTURE=
@@ -21,5 +39,23 @@ function git {
     else
         command git "$@"
     fi
-
 }
+
+alias gO='git push'
+alias gI='git pull'
+alias gOoh='git push -u origin HEAD'
+alias gC='git commit'
+alias gCa='git commit -a'
+alias gco='git checkout'
+alias gcou='git checkout HEAD^'
+alias ga='git add'
+alias gad='git add .'
+alias gap='git add -p'
+alias gs='git stash'
+alias gd='git diff'
+alias gcob='git checkout -b'
+alias gm='git merge'
+alias gr='git reset'
+alias gR='git reset --hard'
+alias grb='git rebase'
+alias gl='git log'
