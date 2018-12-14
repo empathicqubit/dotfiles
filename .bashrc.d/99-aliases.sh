432c30c719477244732555b6e70683cbde6db7aa
empathicqubit <empathicqubit@allons.me>
2018-12-14 12:15:46 -0800

    Typo

--- .bashrc.d/99-aliases.sh
+++ .bashrc.d/99-aliases.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # Random aliases without a home
 function xo {
-    if which open 2>&1 /dev/null ; then
+    if which open 2>&1 >/dev/null ; then
         open "$@"
     else
         xdg-open "$@"
