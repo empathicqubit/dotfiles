5d319b8c3da59f6608866bb6f3b640cb0b56eee3
empathicqubit <empathicqubit@entan.gl>
2024-07-04 12:42:46 +0200

    wallpaper args fix

--- .config/i3/wallpaper.sh
+++ .config/i3/wallpaper.sh
@@ -3,8 +3,13 @@ WALLPAPERDIR="$HOME/Bilder/Wallpapers"
 NUMACTIVE=$(xrandr --listactivemonitors | head -1 | awk '{print $NF}')
 mkdir -p "$WALLPAPERDIR"
 wallargs=()
-find "$WALLPAPERDIR" -type f | shuf -n "$NUMACTIVE" - | while read FILENAME ; do
+while read FILENAME ; do
+    echo "$FILENAME"
     wallargs+=("--bg-fill" "$FILENAME")
-done
-[ -z "${wallargs[@]}" ] && exit 1
+    echo "${wallargs[@]}"
+done < <( find "$WALLPAPERDIR" -type f | shuf -n "$NUMACTIVE" - )
+if [[ "${#wallargs[@]}" -eq 0 ]] ; then
+    exit 1
+fi
+echo feh "${wallargs[@]}"
 feh "${wallargs[@]}"
