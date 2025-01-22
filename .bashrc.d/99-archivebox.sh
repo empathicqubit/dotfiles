f42686345b624ba5ad9d165059e8c5ca4e38733a
empathicqubit <empathicqubit@entan.gl>
2025-01-23 00:31:06 +0100

    Update archivebox command

--- .bashrc.d/99-archivebox.sh
+++ .bashrc.d/99-archivebox.sh
@@ -8,5 +8,5 @@ function archivebox {
         ARGS+=("add" "--extract" "singlefile,title,favicon")
     fi
     ARGS+=("$@")
-    echo CHROME_USER_DATA_DIR="$HOME/.config/archivebox/chrome_profile" CHROME_USER_AGENT='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36' TIMEOUT=120 command archivebox "${ARGS[@]}"
+    CHROME_USER_DATA_DIR="$HOME/.config/archivebox/chrome_profile" CHROME_USER_AGENT='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36' TIMEOUT=120 command archivebox "${ARGS[@]}"
 }
